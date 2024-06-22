e58f1125ace23d46485b7e0ddeffd40e94a222a6
chandhu <chandhudev0@gmail.com>
2024-06-22 16:49:57 +0530

    update: update the eks configurtion iam role identity resource

--- eks/main.tf
+++ eks/main.tf
@@ -1,19 +1,16 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
 resource "aws_iam_role" "eks-role" {
-  name               = "eks-cluster-example"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name = "eks-cluster-example"
+  
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
@@ -43,12 +40,12 @@ resource "aws_eks_cluster" "eks" {
 
   vpc_config {
     endpoint_private_access = false
-    endpoint_public_access = true
-    subnet_ids = [var.private_subnet_ids["us-east-1a"], var.private_subnet_ids["us-east-1b"]]
+    endpoint_public_access  = true
+    subnet_ids              = [var.private_subnet_ids["us-east-1a"], var.private_subnet_ids["us-east-1b"]]
   }
 
   access_config {
-    authentication_mode = "API"
+    authentication_mode                         = "API"
     bootstrap_cluster_creator_admin_permissions = true
   }
 
